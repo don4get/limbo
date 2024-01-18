418ddc635d700b073690a2431c73073996797104
don4get <don4get@users.noreply.github.com>
2024-01-18 20:12:04 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="don4get"
+original_name="limbo"
+original_urlname="limbo"
+original_description="Awesome limbo created by don4get"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv limbo $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
